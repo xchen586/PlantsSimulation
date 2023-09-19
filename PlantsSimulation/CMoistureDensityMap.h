65ea34861867f842b48f8d8bd17a7bb80f8e78bf
VMN\xuanc <xuanc@vmn.net>
2023-09-19 18:57:51 -0400

    Add Moisture and roughness density map.

--- PlantsSimulation/CMoistureDensityMap.h
+++ PlantsSimulation/CMoistureDensityMap.h
@@ -3,25 +3,50 @@
 
 class CMoistureDensityMap : public DensityMap
 {
-
+public:
+	CMoistureDensityMap() : DensityMap()
+	{
+		invert = false;
+		ease = 0.1;
+	}
 };
 
 class COakMoistureDensityMap : public CMoistureDensityMap
 {
-
+public:
+	COakMoistureDensityMap() : CMoistureDensityMap()
+	{
+		minval = 0.15;
+		maxval = 0.7;
+	}
 };
 
 class CMapleMoistureDensityMap : public CMoistureDensityMap
 {
-
+public:
+	CMapleMoistureDensityMap() : CMoistureDensityMap()
+	{
+		minval = 0.35;
+		maxval = 0.8;
+	}
 };
 
 class CBirchMoistureDensityMap : public CMoistureDensityMap
 {
-
+public:
+	CBirchMoistureDensityMap() : CMoistureDensityMap()
+	{
+		minval = 0.25;
+		maxval = 0.8;
+	}
 };
 
 class CFirMoistureDensityMap : public CMoistureDensityMap
 {
-
+public:
+	CFirMoistureDensityMap() : CMoistureDensityMap()
+	{
+		minval = 0.5;
+		maxval = 0.9;
+	}
 };
--- PlantsSimulation/CRoughnessDensityMap.h
+++ PlantsSimulation/CRoughnessDensityMap.h
@@ -3,25 +3,50 @@
 
 class CRoughnessDensityMap : public DensityMap
 {
-
+public:
+	CRoughnessDensityMap() : DensityMap()
+	{
+		invert = false;
+		ease = 0.1;
+	}
 };
 
 class COakRoughnessDensityMap : public CRoughnessDensityMap
 {
-
+public:
+	COakRoughnessDensityMap() : CRoughnessDensityMap()
+	{
+		minval = 0.1;
+		maxval = 0.8;
+	}
 };
 
 class CMapleRoughnessDensityMap : public CRoughnessDensityMap
 {
-
+public:
+	CMapleRoughnessDensityMap() : CRoughnessDensityMap()
+	{
+		minval = 0.2;
+		maxval = 0.7;
+	}
 };
 
 class CBirchRoughnessDensityMap : public CRoughnessDensityMap
 {
-
+public:
+	CBirchRoughnessDensityMap() : CRoughnessDensityMap()
+	{
+		minval = 0.1;
+		maxval = 0.7;
+	}
 };
 
 class CFirRoughnessDensityMap : public CRoughnessDensityMap
 {
-
+public:
+	CFirRoughnessDensityMap() : CRoughnessDensityMap()
+	{
+		minval = 0.35;
+		maxval = 0.9;
+	}
 };
