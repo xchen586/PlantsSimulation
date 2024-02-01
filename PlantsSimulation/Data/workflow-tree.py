456cfabb8d859b789602af434865eb7512883089
sign in <xchen586@gmail.com>
2024-01-31 19:16:30 -0500

    Add new script file and fix a bug.

---
+++ PlantsSimulation/Data/workflow-tree.py
@@ -0,0 +1,105 @@
+from voxelfarm import workflow_lambda
+from voxelfarm import voxelfarmclient
+
+def base_meshes_on_receive_data(
+        vf_api : voxelfarmclient.rest, 
+        request : workflow_lambda.request, 
+        lambda_host : workflow_lambda.workflow_lambda_host):
+    lambda_host.log('Received base meshes')
+    return {'success': True, 'complete': True, 'error_info': 'None'}
+
+def displacement_maps_on_receive_data(
+        vf_api : voxelfarmclient.rest, 
+        request : workflow_lambda.request, 
+        lambda_host : workflow_lambda.workflow_lambda_host):
+    lambda_host.log('Received displacement maps')
+    return {'success': True, 'complete': True, 'error_info': 'None'}
+
+def quadtree_on_receive_data(
+        vf_api : voxelfarmclient.rest, 
+        request : workflow_lambda.request, 
+        lambda_host : workflow_lambda.workflow_lambda_host):
+    lambda_host.log('Received quadtree')
+    return {'success': True, 'complete': True, 'error_info': 'None'}
+
+def world_generation_on_receive_data(
+        vf : voxelfarmclient.rest, 
+        request : workflow_lambda.request, 
+        lambda_host : workflow_lambda.workflow_lambda_host):
+    lambda_host.log('Received world generation data')
+    request.properties['my_property'] = 'my_value'
+
+    result = lambda_host.process_lambda_entity(
+        workflow_request=request,
+        name="Lambda",
+        inputs={
+            'project_id': request.project_id,
+            'msg': 'Hello world'
+        },
+        code='lambda-process.py',
+        files=['lambdas/lambda-process.py'],
+        update_type='msg')
+
+    return {'success': True, 'complete': False, 'error_info': ''}
+
+def world_generation_on_stage_complete(
+        vf_api : voxelfarmclient.rest,
+        request : workflow_lambda.request,
+        lambda_host : workflow_lambda.workflow_lambda_host):
+    
+    update_type = request.update_type
+    lambda_host.log(f'update_type: {update_type}')
+    if update_type == 'msg':
+        lambda_host.log('World generation stage complete')
+        return {'success': True, 'complete': True, 'error_info': 'None'}
+
+    return {'success': True, 'complete': False, 'error_info': 'None'}
+
+lambda_host = workflow_lambda.workflow_lambda_host()
+lambda_host.set_workflow_definition(
+    {
+        'id': 'PANGEA',
+        'name': 'Pangea Next',
+        'icon': 'mesh',
+        'description': 'Pangea workflow',
+        'tracks': [
+            {
+                'id': 'ASSETS',
+                'name': 'Assets',
+                'description': 'The assets to be used in generation',
+                'icon': 'mesh',
+                'tracks': [
+                    {
+                        'id': 'BASE_MESHES',
+                        'name': 'Base Meshes',
+                        'description': 'A collection of OBJ meshes',
+                        'icon': 'mesh',
+                        'on_receive_data': base_meshes_on_receive_data,
+                    },
+                    {
+                        'id': 'DISPLACEMENT_MAPS',
+                        'name': 'Displacement Maps',
+                        'description': 'A collection of PNG heightmaps',
+                        'icon': 'mesh',
+                        'on_receive_data': displacement_maps_on_receive_data,
+                    },
+                    {
+                        'id': 'QUADTREE',
+                        'name': 'Quadtree',
+                        'description': 'A quadtree of the world',
+                        'icon': 'mesh',
+                        'on_receive_data': quadtree_on_receive_data,
+                    },
+                ]
+            },
+            {
+                'id': 'WORLD_GENERATION',
+                'name': 'World Generation',
+                'description': 'The generation of the world',
+                'icon': 'mesh',
+                'on_receive_data': world_generation_on_receive_data,
+                'on_stage_done': world_generation_on_stage_complete,
+            }
+        ],
+    }
+)
