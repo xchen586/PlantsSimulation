37a875fb30b3ccbd6cbb20bf64ba5143cd02cae6
xchen586 <xchen586@gmail.com>
2024-12-05 15:20:46 -0500

    Add script to upload voxel database.

---
+++ PlantsSimulation/Data/xc_upload_db.py
@@ -0,0 +1,306 @@
+import os
+from os import path
+import sys
+import time
+import glob
+
+import shutil
+import subprocess
+import argparse
+import datetime
+
+from datetime import timedelta
+from timeit import default_timer as timer
+#from distutils.dir_util import copy_tree
+from shutil import copytree
+
+import glob
+import configparser
+from io import StringIO
+from zipfile import ZipFile
+
+import pandas as pd
+
+from voxelfarm import voxelfarmclient
+from voxelfarm import workflow_lambda
+from voxelfarm import process_lambda
+
+def exit_code(code):
+    exit(code)
+    
+# Create a custom ConfigParser that preserves case sensitivity
+class CaseSensitiveConfigParser(configparser.ConfigParser):
+    def optionxform(self, optionstr):
+        return optionstr  # Preserve the original case of the key
+    
+def ini_file_to_string(file_path):
+    # Create a ConfigParser object
+    
+    #ConfigParser that lowercase
+    #config = configparser.ConfigParser()
+
+    #ConfigParser that preserves case
+    config = CaseSensitiveConfigParser()
+    
+    # Read the .ini file
+    config.read(file_path)
+
+    # Use a StringIO object to get the content as a string
+    with StringIO() as output:
+        config.write(output)
+        ini_string = output.getvalue()
+
+    return ini_string
+    
+def create_or_update_ini_file(file_path, section, key, value):
+    # Check if the INI file exists
+    if not os.path.exists(file_path):
+        # If not, create the INI file
+        with open(file_path, 'w') as configfile:
+            configfile.write('')
+            
+    # Read the existing INI file
+    
+    #ConfigParser that lowercase
+    #config = configparser.ConfigParser()
+    # Make it not only uppercase
+    #config.optionxform = str
+    
+    #ConfigParser that preserves case
+    config = CaseSensitiveConfigParser()
+    
+    # Use the 'with' statement to ensure proper resource management
+    with open(file_path, 'r') as configfile:
+        config.read_file(configfile)
+    # Check if the section exists, create it if not
+    if not config.has_section(section):
+        config.add_section(section)
+    # Check if the key exists, update it if so, add it if not
+    config.set(section, key, str(value))  # Convert value to string before setting
+    # Write the changes back to the INI file
+    with open(file_path, 'w') as configfile:
+        config.write(configfile)
+
+def read_ini_value(file_path, section, key, default=None, value_type=str):
+    
+    #ConfigParser that lowercase
+    #config = configparser.ConfigParser()
+
+    #ConfigParser that preserves case
+    config = CaseSensitiveConfigParser()
+    
+    # Use the 'with' statement to ensure proper resource management
+    with open(file_path, 'r') as configfile:
+        config.read_file(configfile)
+    # Check if the section and key exist
+    if config.has_section(section) and config.has_option(section, key):
+        # Read the value
+        value = config.get(section, key)
+        # Convert the value to the specified type
+        if value_type == bool:
+            # Convert common string representations of boolean values to bool
+            if value.lower() in ('true', 'yes', 'on', '1'):
+                return True
+            elif value.lower() in ('false', 'no', 'off', '0'):
+                return False
+            else:
+                # Return default if value is not recognized
+                return default
+        else:
+            return value_type(value)
+    else:
+        return default
+
+def clear_all_sections(file_path):
+    # Check if the INI file exists
+    if not os.path.exists(file_path):
+        # If not, create the INI file
+        with open(file_path, 'w') as configfile:
+            configfile.write('')
+    config = configparser.ConfigParser()
+    # Read the existing INI file
+    config.read(file_path)
+    # Remove all sections from the configuration
+    for section in config.sections():
+        config.remove_section(section)
+    # Write the changes back to the INI file
+    with open(file_path, 'w') as configfile:
+        config.write(configfile)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+def swarmDBUploadThread(api : voxelfarmclient.rest, project_id, itemId, outputDBFolder, dbName, dbTitle):
+    try:
+        print("swarmDBUploadThread")
+        
+        OrgId = 2343243456678890
+    
+        # Create configuration string
+        uploaddb_cfg = f'''
+    [Configuration]
+    Organization={OrgId}
+    Instance={itemId}
+    Project={project_id}
+    OutputFolder={outputDBFolder}
+    dbName={dbName}
+    dbTitle={dbTitle}
+    azure_container_name=vfcloudstorage
+    azure_storage_connection_string=DefaultEndpointsProtocol=https;AccountName=vfstpangea;AccountKey=qo+5MnyJBELDbjQUBIOyl7mlyg9FlYnz7XShIyao2wd6Et+vVNMv3Szuvc5uY++zhba8TaWq/uXc+AStuouKIQ==;EndpointSuffix=core.windows.net
+    storage_provider=AZURE
+    '''
+
+        # Log the configuration string
+        print(f'XC Tool.UploadDB.exe surveys configuration ini file content is : ')
+        print(f'{uploaddb_cfg}')
+        
+        # Create path for the ini file
+        ini_file = os.path.join(Data_folder, f'uploaddb_{dbName}_{itemId}.ini')
+        print(f'XC Create surveys configuration file {ini_file}')
+        
+        # Write configuration string to the ini file
+        with open(ini_file, "w") as ini:
+            ini.write(uploaddb_cfg)
+        
+        # Log the configuration string again
+        print(f'{uploaddb_cfg}')
+        
+        # Define swarm index and size
+        swarmIndex = 0
+        swarmSize = 1
+    
+        # Create the command to run the external process
+        command = [
+            f'{tools}\\Tool.UploadDB.exe', 
+            ini_file,
+            str(swarmIndex), 
+            str(swarmSize)
+        ]
+        
+        print("Command arguments: " + " ".join(command))
+        
+        # Start the process
+        process = subprocess.Popen(
+            command,
+            cwd=tools,  # Working directory
+            stdin=subprocess.PIPE,  # Redirecting input
+            stdout=subprocess.PIPE,  # Redirecting output
+            stderr=subprocess.PIPE,  # Redirecting errors (optional)
+            text=True  # This allows reading/writing strings instead of bytes
+        )
+        
+        entity = api.get_entity(itemId)
+        
+        is_virtual = False
+        virtual_value = entity['virtual']
+        if virtual_value == "1":
+            is_virtual = True
+
+        if not is_virtual:
+            # Read the output line by line (non-blocking)
+            for line in process.stdout:
+                print(f'{itemId}, {project_id}, {line.strip()}')
+
+        # Wait for the process to exit
+        process.wait()
+
+        # Check the exit code
+        success = process.returncode == 33
+        print("UploadDB.exe exit code: " + str(process.returncode))
+
+        if success:
+            print(f'swarmDBUploadThread is successful!')
+        else:
+            print(f'swarmDBUploadThread is failed!')
+            
+        print("swarmDBUploadThread done")
+        return process.returncode
+
+    except Exception as e:
+        print(f"Tool.UploadDB.exe Error: {str(e)}")
+        return False
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def do_simple_upload_basemeshes_swarm(api : voxelfarmclient.rest, project_id, basemeshes_db_folderId, file_path : str, version : int, entity_name : str, code_path : str):
+    print(f'Start do_simple_upload_basemeshes_swarm Created entity {entity_name}')
+
+    result = api.get_project_crs(project_id)
+    crs = result.crs
+    entity_id = None
+
+    print(f'start create_entity_raw file for entity {entity_name}')
+    result = api.create_entity_raw(project=project_id, 
+        type=api.entity_type.VoxelPC, 
+        name=entity_name, 
+        fields={
+            'state': 'PARTIAL',
+            'file_folder': basemeshes_db_folderId,
+            'virtual': '0',
+        }, crs = crs)
+    entity_id = result.id
+    print(f'end create_entity_raw file for entity {entity_name}')
+    if not result.success:
+        print(f'Fail to create_entity_raw Created entity for {entity_name} : {result.error_info}')
+    else:
+        print(f'Successfully to create_entity_raw Created entity for {result.id} for {entity_name}')
+        
+    #dbName = f'vox-mesh-{entity_name}'
+    #dbTitle = f'Voxel Mesh Data For {entity_name}'
+    print(f'Start to do_swarm_db_upload entity_id : {entity_id} ---- with folder ; {file_path}')
+    
+    uploadcode = None
+    try:
+        # Attempt to upload the database
+        #dbName = f'vox-mesh-{entity_name}'
+        #dbTitle = f'Voxel Mesh Data For {entity_name}'
+        dbName = f'vox-pc'
+        dbTitle = f'Voxel Data'
+        #uploadcode = do_swarm_db_upload(project_id, entity_id, file_path, dbName, dbTitle)
+        uploadcode = swarmDBUploadThread(api, project_id, entity_id, file_path, dbName, dbTitle)
+    except Exception as e:
+        # Log any exceptions that occur during the upload
+        print(f'Exception during do_swarm_db_upload: files folder: {file_path} to entity {entity_id} with exception: {str(e)}')
+    
+    # Check if uploadcode was assigned
+    if uploadcode is not None:
+        # Proceed with further logic if needed
+        print(f'Upload completed with exit code: {uploadcode}')
+    else:
+        # Handle the case where uploadcode was not assigned (likely due to an exception)
+        print('Upload did not complete successfully. Please check the logs for details.')
+
+    result = api.update_entity(
+    id=entity_id,
+    project=project_id, 
+    fields={
+        'state' : 'COMPLETE'
+    })
+
+    
+    if not result.success:
+        print(result.error_info)
+        exit_code(111)
+
+    print(f'End do_simple_upload_basemeshes_swarm Created entity {result.id} for {entity_name}')
+
+pythoncode_data_folder = f'D:\\Downloads\\XCTreeWorkFlow\\PythonCode'
+workflow_project_id = '1D4CBBD1D957477E8CC3FF376FB87470' #Pangea Next Project
+scrap_folder= f'D:\\Downloads\\XCTreeCreation'
+tools = f'D:\\Downloads\\XCTreeWorkFlow\\Tools'
+Tree_Data_Folder_Name = 'Tree_Big_Creation'
+Data_folder = os.path.join(scrap_folder, Tree_Data_Folder_Name)
+cloud_url = 'https://demo.voxelfarm.com/'
+Project_id = workflow_project_id
+api = voxelfarmclient.rest(cloud_url)
+
+tiles = 25
+x = 8
+y = 5
+
+basemeshes_db_folder_Id = '75FDBF01261147F3A50E4A6CFDE059D3' 
+db_output_folder = f'D:\\Downloads\\XCTreeCreation\\Tree_Big_Creation\\cavesdb\\{tiles}_{x}_{y}'
+
+
+project_entity = api.get_entity(Project_id)
+version = int(project_entity['version']) + 1 if 'version' in project_entity else 1
+entity_name = f'TopCaves_{tiles}_{x}_{y}-{version}'
+
+do_simple_upload_basemeshes_swarm(api, Project_id, basemeshes_db_folder_Id, db_output_folder, version, entity_name, pythoncode_data_folder)
