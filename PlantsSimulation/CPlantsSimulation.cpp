4fee8846a9bc25a7e0ddbfad5ee994a827dc824e
xchen586 <xchen586@gmail.com>
2025-11-19 18:24:22 -0500

    Adjust argument for export blurred heightmap.

--- PlantsSimulation/CPlantsSimulation.cpp
+++ PlantsSimulation/CPlantsSimulation.cpp
@@ -1203,8 +1203,8 @@ bool CPlantsSimulation::LoadInputHeightMap()
 	//std::vector<std::vector<short>> heightmapNormalBlurShort4096 = NormalGaussianBlurHeightmap(heightMapShort4096, 15, 5.0);
 	//std::vector<std::vector<short>> heightmapIIRBlurShort4096 = IIRGaussianBlurHeightmap(heightMapShort4096, 0, 8.0);
 
-	std::vector<std::vector<short>> heightmapNormalBlurShort4096 = MaskedGaussianBlurHeightmap(heightMapShort4096, heightMasksShort4096, 15, 5.0);
-	std::vector<std::vector<short>> heightmapIIRBlurShort4096 = iir_gauss_blur_with_mask(heightMapShort4096, heightMasksShort4096, 15);
+	std::vector<std::vector<short>> heightmapNormalBlurShort4096 = MaskedGaussianBlurHeightmap(heightMapShort4096, heightMasksShort4096, 6, 2.0);
+	std::vector<std::vector<short>> heightmapIIRBlurShort4096 = iir_gauss_blur_with_mask(heightMapShort4096, heightMasksShort4096, 5);
 
 	//std::vector<std::vector<short>> slopeShort4096 = ComputeAbsMaxHeightSlopeMap(heightMapShort4096);
 	std::vector<std::vector<short>> slopeShort4096 = ComputeSlopeMap(heightMapShort4096);
